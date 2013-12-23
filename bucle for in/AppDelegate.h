0ef74a345f7a75f7a8c1ce772c0132d1ed0eba60
rober <gascon87@gmail.com>
2013-12-23 19:30:00 +0100

    Initial Commit

---
+++ bucle for in/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bucle for in
+//
+//  Created by rober on 23/12/13.
+//  Copyright (c) 2013 rogama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
